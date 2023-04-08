390b57badad53929d537b7d84f6c343d30f88da9
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2023-04-07 23:43:26 -0300

    Atualização da url do servidor backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   //apiServer: 'https://localhost:44332/'
-  apiServer: 'http://academico3.rj.senac.br:8080/'
-  //apiServer: 'http://academico3.senacrjedu.local:8080/'
+  apiServer: 'http://academico3.rj.senac.br/'
 };
 
 /*
