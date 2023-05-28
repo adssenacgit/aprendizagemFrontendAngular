559cdbf7288dceb6304304cfc3d31bbf5d1d9113
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-05-28 04:16:43 -0300

    refactor: alterações para a realização do upload e do roteamento com o dashboard(lista de tarefas)

--- src/app/components/UnidadeCurricular/estudo-previo/estudo-previo.component.ts
+++ src/app/components/UnidadeCurricular/estudo-previo/estudo-previo.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Atividade } from 'src/app/models/Atividade';
 import { SituacaoAprendizagem } from 'src/app/models/SituacaoAprendizagem';
 import { UnidadeCurricular } from 'src/app/models/UnidadeCurricular';
 import { AtividadeService } from 'src/app/services/atividade.service';
+import { AuthGuardService } from 'src/app/services/auth-guard.service';
 import { EstudoPrevioService } from 'src/app/services/estudo-previo.service';
+import { SituacaoAprendizagemService } from 'src/app/services/situacaoaprendizagem.service';
 import { UnidadeCurricularService } from 'src/app/services/unidade-curricular.service';
 
 @Component({
@@ -14,35 +17,82 @@ import { UnidadeCurricularService } from 'src/app/services/unidade-curricular.se
 })
 
 export class EstudoPrevioComponent implements OnInit {
-    loading: boolean= true;
-    constructor(private router: Router,
-      private route: ActivatedRoute,
-      private estudoPrevioService : EstudoPrevioService,
-      private atividadeService: AtividadeService) {
-    }
-    teste : string;
-    ngOnInit(): void {
-      this.situacaoId = this.route.snapshot.params['id'];
-      // this.situacaoId = 1;
-      this.estudoPrevioService.ObterSituacaoAprendizagem(this.situacaoId).subscribe(resultado => {
+  loading: boolean = true;
+  selectedFile: File | null;
+  files: Set<File>;
+  idEstudanteUsuarioLogado: number;
+  idAtividade: number;
+  teste: string;
+  inscricao: Subscription;
+  atividades: Atividade[];
+  atividadeVerificacao: Atividade;
+  idSituacao: number;
+  situacaoAprendizagem: SituacaoAprendizagem;
+
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private estudoPrevioService: EstudoPrevioService,
+    private atividadeService: AtividadeService,
+    private authGuardService: AuthGuardService,
+    private situacaoAprendizagemService: SituacaoAprendizagemService,
+  ) {
+  }
+  ngOnInit(): void {
+
+    this.idEstudanteUsuarioLogado = this.authGuardService.getIdEstudanteUsuarioLogado();
+
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.idAtividade = params['id']
+
+        this.obterAtividade();
+
+        this.obterSituacaoAprendizagem();
+      }
+    )
+  }
+
+  comecarAtividade() {
+    this.router.navigate(['/cursos/atividades/' + this.situacaoAprendizagem.id])
+  }
+
+  obterAtividade = () => {
+    this.atividadeService.ObterAtividadePorSituacaoId(this.idAtividade)
+      .subscribe((resultado) => {
+        this.atividadeVerificacao = resultado;
+        console.log(this.atividadeVerificacao);
+      });
+  };
+
+  obterSituacaoAprendizagem = () => {
+    this.situacaoAprendizagemService.ObterSituacaoAprendizagemPorId(this.idSituacao)
+      .subscribe((resultado) => {
         this.situacaoAprendizagem = resultado;
-        
-        this.atividadeService.ObterAtividadePorSituacaoId(this.situacaoAprendizagem.id).subscribe(atv =>{
-          this.atividades = atv;
-        })
-        this.atividadeService.formatarAtividades(this.atividades).then(atv => {
-          this.atividades = atv;
-        })
-      })
-      this.loading = false;
-      this.teste = "teste"
+        console.log(this.situacaoAprendizagem);
+      });
+  };
+
+  onFileSelected($event: any) {
+    const selectedFile = $event.target.files;
+    const fileName = [];
+    this.files = new Set();
+    for (let index = 0; index < selectedFile.length; index++) {
+      fileName.push(selectedFile[index].name);
+      this.files.add(selectedFile[index]);
     }
-    atividades : Atividade[] = []
-    situacaoId : number;
-    situacaoAprendizagem : SituacaoAprendizagem;
-  
-    comecarAtividade(){
-      
-      this.router.navigate(['/cursos/atividades/'+this.situacaoAprendizagem.id])
+    fileName.join(", ");
+    console.log($event.target.files);
+  }
+
+  enviarArquivo() {
+    if (this.files && this.files.size > 0) {
+      this.atividadeService.CadastrarAtividade(this.files, this.idAtividade, this.idEstudanteUsuarioLogado)
+        .subscribe(() => { console.log("Upload concluído!") });
     }
   }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+
+}
