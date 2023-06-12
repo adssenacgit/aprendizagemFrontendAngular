6827aaddfeec8ed172b5a4272711bf990c1d7d18
Sartori11 <sartori.pedro19@gmail.com>
2023-06-12 13:11:39 -0300

    novapergunta

--- src/app/components/ApoioDuvidas/novapergunta/novapergunta.component.ts
+++ src/app/components/ApoioDuvidas/novapergunta/novapergunta.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Chapter } from 'src/app/models/Chapter';
+import { ChapterAssunto } from 'src/app/models/ChapterAssunto';
+import { Usuario } from 'src/app/models/Usuario';
+import { ChapterAssuntoService } from 'src/app/services/chapter-assunto.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-novapergunta',
@@ -7,9 +13,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovaPerguntaComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  submitted = false;
+  pergunta: ChapterAssunto;
+
+
+
+  constructor(
+    private fb:FormBuilder,private service: ChapterAssuntoService,private serviceUsuario: UsuariosService) { }
 
   ngOnInit(): void {
-  }
+    this.form = this.fb.group({
+      descricao: [null,[Validators.required,Validators.minLength(5)]]
+    });
+    
 
+       
+   
+ }    
+ hasError(field: string) {
+      return this.form.get(field)?.errors;
+    }
+
+  
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.form.value);
+    if(this.form.valid) {
+      console.log("Submit");
+    
+
+      
+      this.pergunta.descricao = this.form.value;
+
+
+      this.service.NovoChapterAssunto(this.pergunta).subscribe(
+        sucess => console.log('sucesso'),
+        error => console.error(error),
+        () => console.log('request completo')
+      );
+    }
+      
+    }
+
+  onCancel() {
+    console.log("onCancel");
+    this.form.reset();
+
+
+  }
 }
