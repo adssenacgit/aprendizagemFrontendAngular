cc525d4658b108d05f78c137f9b2554d930d0abf
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-06-17 23:34:41 -0300

    reafctor: settings in trilha do curso

--- src/app/components/Dashboard/usuario-dashboard/usuario-trilha-curso/usuario-trilha-curso.component.ts
+++ src/app/components/Dashboard/usuario-dashboard/usuario-trilha-curso/usuario-trilha-curso.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { delay, catchError, of } from 'rxjs';
+import { Modulo } from 'src/app/models/Modulo';
 import { SenacCoin } from 'src/app/models/SenacCoin';
 import { SenacCoinMovimentacao } from 'src/app/models/SenacCoinMovimentacao';
+import { UnidadeCurricular } from 'src/app/models/UnidadeCurricular';
+import { AuthGuardService } from 'src/app/services/auth-guard.service';
+import { ModuloService } from 'src/app/services/modulo.service';
 import { SenacCoinMovimentacaoService } from 'src/app/services/senac-coin-movimentacao.service';
 import { SenacCoinService } from 'src/app/services/senac-coin.service';
+import { UnidadeCurricularService } from 'src/app/services/unidade-curricular.service';
+import { ErrorService } from 'src/app/shared/alerts/error-dialog/error-dialog.service';
 
 @Component({
   selector: 'app-trilha-curso',
@@ -11,26 +18,38 @@ import { SenacCoinService } from 'src/app/services/senac-coin.service';
 })
 export class UsuarioTrilhaCursoComponent implements OnInit {
 
-  senacCoins : SenacCoin;
+  senacCoins: SenacCoin;
   loading: boolean = true;
-  senacCoinMovimentacoes : SenacCoinMovimentacao[]; 
-  idUsuarioLogado : string;
+  senacCoinMovimentacoes: SenacCoinMovimentacao[];
+  idEstudanteUsuarioLogado: number;
+  modulos: Modulo[];
 
-  constructor(private senacCoinService : SenacCoinService, private senacCoinMovimentacao : SenacCoinMovimentacaoService ) { }
+  constructor(
+    private authGuardService: AuthGuardService,
+    private unidadeCurricularService: UnidadeCurricularService,
+    private errorService: ErrorService,
+    private moduloService: ModuloService,
+  ) { }
 
   ngOnInit(): void {
-   
-    this.idUsuarioLogado = localStorage.getItem("UsuarioId")!;
 
-    this.senacCoinService.ObterSenacCoinPeloUsuarioId(this.idUsuarioLogado).subscribe((resultado:SenacCoin) =>{
-      this.senacCoins = resultado;
-      this.loading = false;
+    this.idEstudanteUsuarioLogado = this.authGuardService.getIdEstudanteUsuarioLogado();
+
+    this.ObterModulos();
+
+  }
+
+  ObterModulos = () => {
+    this.moduloService.ObterModulosPorEstudanteId(this.idEstudanteUsuarioLogado).pipe(
+      delay(500),
+      catchError((error) => {
+        this.errorService.onError('Erro ao carregar unidades curriculares.');
+        return of([]);
+      })
+    ).subscribe((resultadoUnidadesCurriculares) => {
+      this.modulos = resultadoUnidadesCurriculares;
+      console.log(this.idEstudanteUsuarioLogado)
+      console.log(this.modulos)
     });
-
-    this.senacCoinMovimentacao.ObterSenacCoinMovimentacaoPeloUsuarioId(this.idUsuarioLogado).subscribe((resultado:SenacCoinMovimentacao[]) =>{
-        this.senacCoinMovimentacoes = resultado;
-        this.loading = false;
-    }); 
-
   }
 }
