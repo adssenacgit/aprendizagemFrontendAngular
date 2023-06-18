b3a1be36535719fc55dfb68ccae56e3b95650c1b
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-06-17 22:47:45 -0300

    reafctor: seeting in unidade curricular service

--- src/app/services/unidade-curricular.service.ts
+++ src/app/services/unidade-curricular.service.ts
@@ -6,11 +6,11 @@ import { environment } from 'src/environments/environment';
 
 
 const httpOptions = {
-  headers: new HttpHeaders ({
+  headers: new HttpHeaders({
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${localStorage.getItem('TokeUsuarioLogado')}`,
-    }),
-  };
+  }),
+};
 
 @Injectable({
   providedIn: 'root'
@@ -20,20 +20,17 @@ export class UnidadeCurricularService {
   url = environment.apiServer + 'api/UnidadeCurricular';
   constructor(private https: HttpClient) { }
 
-  ObterUnidadeCurricularPeloId(ucId: number): Observable<UnidadeCurricular>
-  {
+  ObterUnidadeCurricularPeloId(ucId: number): Observable<UnidadeCurricular> {
     const apiUrl = `${this.url}/${ucId}`;
     return this.https.get<UnidadeCurricular>(apiUrl);
   }
 
-  ObterUnidadeCurricularPelaOfertaId (ofertaId: number) : Observable<UnidadeCurricular[]>
-  {
+  ObterUnidadeCurricularPelaOfertaId(ofertaId: number): Observable<UnidadeCurricular[]> {
     const apiUrl = `${this.url}/filterByOferta/${ofertaId}`;
     return this.https.get<UnidadeCurricular[]>(apiUrl);
   }
 
-  ObterUnidadeCurricularPeloModuloId (moduloId: number) : Observable<UnidadeCurricular[]>
-  {
+  ObterUnidadeCurricularPeloModuloId(moduloId: number): Observable<UnidadeCurricular[]> {
     const apiUrl = `${this.url}/filterByModulo/${moduloId}`;
     return this.https.get<UnidadeCurricular[]>(apiUrl);
   }
@@ -44,29 +41,38 @@ export class UnidadeCurricularService {
   //   return this.https.get<UnidadeCurricular[]>(apiUrl);
   // }
 
-  // ObterUnidadeCurricularPeloUsuarioId (id: string, idPeriodo: number) : Observable<UnidadeCurricular[]>
-  // {
-  //   const apiUrl = `${this.url}/filterByUsuarioId/${id}/${idPeriodo}`;
-  //   return this.https.get<UnidadeCurricular[]>(apiUrl);
-  // }
+  ObterUnidadesCurriculares(): Observable<UnidadeCurricular[]> {
+    const apiUrl = `${this.url}`;
+    return this.https.get<UnidadeCurricular[]>(apiUrl);
+  }
 
-  NovaUnidadeCurricular(uc: UnidadeCurricular): Observable<any>{
+  ObterUnidadeCurricularPeloUsuarioId(id: string, idPeriodo: number): Observable<UnidadeCurricular[]> {
+    const apiUrl = `${this.url}/filterByUsuarioId/${id}/${idPeriodo}`;
+    return this.https.get<UnidadeCurricular[]>(apiUrl);
+  }
+
+  NovaUnidadeCurricular(uc: UnidadeCurricular): Observable<any> {
     return this.https.post<UnidadeCurricular>(this.url, uc, httpOptions);
   }
 
-  AtualizarUnidadeCurricular(ucId: number, uc: UnidadeCurricular): Observable<any>{
+  AtualizarUnidadeCurricular(ucId: number, uc: UnidadeCurricular): Observable<any> {
     const apiUrl = `${this.url}/${ucId}`;
     return this.https.put<UnidadeCurricular>(apiUrl, uc, httpOptions);
   }
 
-  ExcluirUnidadeCurricular(ucId: number): Observable<any>{
+  ExcluirUnidadeCurricular(ucId: number): Observable<any> {
     const apiUrl = `${this.url}/${ucId}`;
-    return this.https.delete<UnidadeCurricular>(apiUrl, httpOptions);    
+    return this.https.delete<UnidadeCurricular>(apiUrl, httpOptions);
   }
 
-  FiltrarUnidadeCurricular(nomeUc: string, idOferta: number) : Observable<UnidadeCurricular[]>
-  {
+  FiltrarUnidadeCurricular(nomeUc: string, idOferta: number): Observable<UnidadeCurricular[]> {
     const apiUrl = `${this.url}/FiltrarUnidadeCurricular/${idOferta}/${nomeUc}`;
+    return this.https.get<UnidadeCurricular[]>(apiUrl);
+  }
+
+  ObterTodasUnidadesCurriculares(): Observable<UnidadeCurricular[]>
+  {
+    const apiUrl = `${this.url}`;
     return this.https.get<UnidadeCurricular[]>(apiUrl);
   }
 
