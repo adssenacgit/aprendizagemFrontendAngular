c855e82b3ffa39de316ab468182ed1e31b07c259
Sartori11 <sartori.pedro19@gmail.com>
2023-06-19 21:18:37 -0300

    comentariologica

--- src/app/services/comentario.service.ts
+++ src/app/services/comentario.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ChapterAssuntoComentario } from 'src/app/models/ChapterAssuntoComentario';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders ({
+    'Content-Type' : 'application/json',
+    'Authorization': `Bearer ${localStorage.getItem('TokenUsuarioLogado')}`
+  }),
+};
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +24,10 @@ export class ComentarioService {
     const apiUrl = `${this.url}/filterByChapterAssuntoId/${id}`;
     let teste = this.https.get<ChapterAssuntoComentario[]>(apiUrl)
     return teste;
+  }
+
+  NovoChapterAssuntoComentario (comentario: ChapterAssuntoComentario): Observable<ChapterAssuntoComentario>
+  {
+    return this.https.post<ChapterAssuntoComentario>(this.url, comentario, httpOptions);
   }
 }
