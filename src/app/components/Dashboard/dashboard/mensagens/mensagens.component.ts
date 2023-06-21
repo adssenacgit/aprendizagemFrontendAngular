52945ca063042f2bc6fa97558b25803a391452d8
vtr363 <victor.m.rocha.vr@gmail.com>
2023-06-21 20:30:40 -0300

    listagem de contatos

--- src/app/components/Dashboard/dashboard/mensagens/mensagens.component.ts
+++ src/app/components/Dashboard/dashboard/mensagens/mensagens.component.ts
@@ -1,7 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ContentChildDecorator, Input, OnInit } from '@angular/core';
+import { isEmpty } from 'rxjs';
+import { Estudante } from 'src/app/models/Estudante';
 import { Mensagem } from 'src/app/models/Mensagem';
 import { Usuario } from 'src/app/models/Usuario';
 import { AuthGuardService } from 'src/app/services/auth-guard.service';
+import { EstudantesService } from 'src/app/services/estudante.service';
+import { MensagemService } from 'src/app/services/mensagem.service';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
@@ -61,28 +65,78 @@ export class MensagensComponent implements OnInit {
     }
   ]
 
-  mensagens = [1,2,3,4, 6, 7,8,9,10];
 
   activeIndex = 0;
   chat = false;
-  usuario : Usuario = new Usuario;
+  estudante: Estudante;
+  contatos: Usuario[] = [];
+  contatoSelecionado: Usuario;
+  mensagens: Mensagem[];
   idUsuarioLogado : string;
+  usuario: Usuario;
 
   constructor(
-    private usuarioService : UsuariosService,
+    private usuarioService: UsuariosService,
+    private mensagemService : MensagemService,
+    private estudanteService : EstudantesService,
     private authGuardService: AuthGuardService
     ) { }
 
   ngOnInit(): void {
     this.idUsuarioLogado = this.authGuardService.getIdUsuarioLogado();
 
-    this.usuarioService.ObterUsuarioPorId(this.idUsuarioLogado).subscribe(resultado=>{
+    this.estudanteService.ObterEstudanteByUsuarioId(this.idUsuarioLogado).subscribe(resultado=>{
+      this.estudante = resultado;
+    })
+
+    // this.estudanteService.ObterEstudanteByOfertaId(this.estudante.ofertaId).subscribe(resultado=>{
+    this.estudanteService.ObterEstudanteByOfertaId(1).subscribe(resultado=>{
+    
+    resultado.forEach(estudante => {
+      this.contatos.push(estudante.usuario)
+    });
+
+    // Ordena a lista
+    this.contatos.sort((a,b) => (a.nomeCompleto > b.nomeCompleto) ? 1 : ((b.nomeCompleto > a.nomeCompleto) ? -1 : 0))
+      
+    this.usuarioService.ObterUsuarioPorId(this.idUsuarioLogado).subscribe(resultado => {
       this.usuario = resultado;
+    })
+
     })
   }
 
-  toggleChat(){
+ 
+
+  toggleChat(usuario?: Usuario){
+    
     this.chat = !this.chat
+    if(usuario){
+      this.contatoSelecionado = usuario
+  
+      this.mensagemService.ObterMensagemPorUsuarioId(this.contatoSelecionado.id).subscribe(resultado => {
+        this.mensagens = resultado
+      })
+      this.mensagemService.ObterMensagemPorUsuarioId(this.idUsuarioLogado).subscribe(resultado => {
+        resultado.forEach(mensagem => {
+          this.mensagens.push(mensagem)
+        });
+      })
+      this.mensagens.sort((a,b) => (a.data > b.data) ? 1 : ((b.data > a.data) ? -1 : 0))
+    }
+    console.log("oi",this.mensagens)
+
+  }
+
+  EnviarMensagem(mensagemTexto: string){
+    let mensagemEnviada: Mensagem = new Mensagem;
+    mensagemEnviada.data = Date.now().toString();
+    mensagemEnviada.mensagemTexto = mensagemTexto;
+    mensagemEnviada.status = 0
+    mensagemEnviada.usuarioAutor = this.usuario;
+    mensagemEnviada.usuarioIdAutor = this.usuario.id;
+    mensagemEnviada.usuario = this.contatoSelecionado;
+    mensagemEnviada.usuarioId = this.contatoSelecionado.id;
 
   }
 
