76ce8f584eb7366c3e6b78fa836b808385bb3a7b
Sartori11 <sartori.pedro19@gmail.com>
2023-06-21 20:06:16 -0300

    paginacao

--- src/app/components/ApoioDuvidas/apoio-duvidas.component.ts
+++ src/app/components/ApoioDuvidas/apoio-duvidas.component.ts
@@ -11,6 +11,10 @@ import { ChapterAssuntoService } from 'src/app/services/chapter-assunto.service'
 export class ApoioDuvidasComponent implements OnInit {
   
   chapterAssuntos: ChapterAssunto[];
+  currentPage: number = 1;
+  itemsPerPage: number = 4;
+  startIndex: number = (this.currentPage - 1) * this.itemsPerPage;
+  endIndex: number = this.currentPage * this.itemsPerPage;
 
   constructor(
     private _route: ActivatedRoute,
@@ -21,5 +25,24 @@ export class ApoioDuvidasComponent implements OnInit {
     this.chapterAssuntoService.ObterTodos().subscribe((data) => {
       this.chapterAssuntos = data;
     });
+  }
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePage();
+    }
+  }
+  
+  nextPage() {
+    const totalPages = Math.ceil(this.chapterAssuntos.length / this.itemsPerPage);
+    if (this.currentPage < totalPages) {
+      this.currentPage++;
+      this.updatePage();
+    }
+  }
+  
+  updatePage() {
+    this.startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    this.endIndex = Math.min(this.currentPage * this.itemsPerPage, this.chapterAssuntos.length);
   }
 }
