67db9808eebc0767267d5fc10075c4ccd7044c97
Daniel Dantas <contatodanieldantasdev@gmail.com>
2023-10-03 08:56:23 -0300

    refactor: implements dynamic funcionality

--- src/app/components/Dashboard/usuario-dashboard/cards-senac-coin/cards-senac-coin.component.ts
+++ src/app/components/Dashboard/usuario-dashboard/cards-senac-coin/cards-senac-coin.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { cardsData } from './cards-data';
+import {Component, Input, OnInit} from '@angular/core';
 import { SenacCoinMovimentacao } from 'src/app/models/SenacCoinMovimentacao';
 import { AuthGuardService } from 'src/app/services/auth-guard.service';
 import { SenacCoinMovimentacaoService } from 'src/app/services/senac-coin-movimentacao.service';
@@ -11,46 +10,7 @@ import { SenacCoin } from 'src/app/models/SenacCoin';
   templateUrl: './cards-senac-coin.component.html',
   styleUrls: ['./cards-senac-coin.component.css'],
 })
-export class CardsSenacCoinComponent implements OnInit {
-  cardsData = cardsData;
-  itensComprados: any[] = [];
-  itemAtivo: boolean = true;
-  loading: boolean = true;
-  idUsuarioLogado: string;
-  senacCoins: SenacCoin = new SenacCoin();
-  senacCoinMovimentacoes: SenacCoinMovimentacao[];
-
-  constructor(
-    private senacCoinService: SenacCoinService,
-    private senacCoinMovimentacao: SenacCoinMovimentacaoService,
-    private authGuardService: AuthGuardService
-  ) {}
-
-  ngOnInit() {
-    this.idUsuarioLogado = this.authGuardService.getIdUsuarioLogado();
-    this.senacCoinService
-      .ObterSenacCoinPeloUsuarioId(this.idUsuarioLogado)
-      .subscribe((resultado) => {
-        this.senacCoins = resultado;
-        this.loading = false;
-      });
-
-    this.senacCoinMovimentacao
-      .ObterSenacCoinMovimentacaoPeloUsuarioId(this.idUsuarioLogado)
-      .subscribe((resultado) => {
-        this.senacCoinMovimentacoes = resultado;
-        this.loading = false;
-      });
-  }
-
-  // comprarItem(item: Item): void {
-  //   if (this.senacCoins.saldo >= senacCoinsCatalogo.valor) {
-  //     this.senacCoins.saldo -= senacCoinsCatalogo.valor;
-  //     this.itensComprados.push(item);
-  //     console.log(`Item '${item.nome}' comprado com sucesso!`);
-  //   } else {
-  //     console.log(`Saldo insuficiente para comprar o item '${item.nome}'.`);
-  //   }
-  // }
-
+export class CardsSenacCoinComponent {
+  @Input() $senacCoins: SenacCoin[] = [];
+  @Input() $senacCoinMovimentacoes: SenacCoinMovimentacao[] = [];
 }
