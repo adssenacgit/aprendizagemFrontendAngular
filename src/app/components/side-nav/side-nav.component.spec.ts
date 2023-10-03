7e99753d18aea66730754a5cf793bd02b2205d5a
victorfonsecabarboza <victorfonsecabarboza@gmail.com>
2023-10-03 15:25:34 -0300

    initial commit

--- src/app/components/side-nav/side-nav.component.spec.ts
+++ src/app/components/side-nav/side-nav.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SideNavComponent } from './side-nav.component';
+// import { SideNavComponent } from './side-nav.component';
 
-describe('SideNavComponent', () => {
-  let component: SideNavComponent;
-  let fixture: ComponentFixture<SideNavComponent>;
+// describe('SideNavComponent', () => {
+//   let component: SideNavComponent;
+//   let fixture: ComponentFixture<SideNavComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SideNavComponent ]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ SideNavComponent ]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(SideNavComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(SideNavComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
