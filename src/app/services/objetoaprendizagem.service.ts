3c24c73d948c0ec8ef336f404527aa6b22c4e150
Felipe Sanmartin <lipesanmartin@gmail.com>
2023-10-03 01:53:32 -0300

    CRUD ObjetoAprendizagem
    criado de acordo com o swagger dia 03/10/23

--- src/app/services/objetoaprendizagem.service.ts
+++ src/app/services/objetoaprendizagem.service.ts
@@ -21,9 +21,44 @@ export class ObjetoAprendizagemService {
   url = environment.apiServer + 'api/ObjetoAprendizagem';
   constructor(private https: HttpClient) { }
 
+  criarObjetoAprendizagem(objeto: ObjetoAprendizagem): Observable<ObjetoAprendizagem>
+  {
+    return this.https.post<ObjetoAprendizagem>(this.url, objeto);
+  }
+
+  obterObjetosAprendizagem(): Observable<ObjetoAprendizagem[]> {
+    return this.https.get<ObjetoAprendizagem[]>(this.url);
+  }
+
+  obterObjetoAprendizagemPorId(id: number): Observable<ObjetoAprendizagem> {
+    return this.https.get<ObjetoAprendizagem>(`${this.url}/${id}`);
+  }
+
+  atualizarObjetoAprendizagem(id: number, objeto: ObjetoAprendizagem): Observable<ObjetoAprendizagem>
+  {
+    return this.https.put<ObjetoAprendizagem>(`${this.url}/${id}`, objeto);
+  }
+
+  excluirObjetoAprendizagem(id: number): Observable<void> {
+    return this.https.delete<void>(`${this.url}/${id}`);
+  }
+
   FiltrarObjetoAprendizagemPorSituacaoAprendizagemId (id: number) : Observable<ObjetoAprendizagem[]>
   {
     const apiUrl = `${this.url}/FiltrarObjetoAprendizagemBySituacaoAprendizagemId/${id}`;
     return this.https.get<ObjetoAprendizagem[]>(apiUrl);
   }
+
+  FiltrarObjetoAprendizagemPorIndicadorCompetenciaId (id: number) : Observable<ObjetoAprendizagem[]>
+  {
+    const apiUrl = `${this.url}/FiltrarObjetoAprendizagemByIndicadorCompetenciaId/${id}`;
+    return this.https.get<ObjetoAprendizagem[]>(apiUrl);
+  }
+
+  FiltrarObjetoAprendizagemPorDescricao (descricao: string) : Observable<ObjetoAprendizagem[]>
+  {
+    const apiUrl = `${this.url}/FiltrarObjetoAprendizagem/${descricao}`;
+    return this.https.get<ObjetoAprendizagem[]>(apiUrl);
+  }
+
 }
