4c5cd3a5a63cbad81495150655f9ef91759df131
Guilherme Valim <36124872022.2N@senacrjedu.local>
2023-10-27 19:00:37 -0300

    Alterações dashboard-professor

--- src/app/components/Dashboard/professor-dashboard/professor-dashboard.component.ts
+++ src/app/components/Dashboard/professor-dashboard/professor-dashboard.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthGuardService } from 'src/app/services/auth-guard.service';
-import { Grupo } from 'src/app/models/Grupo';
-import { GrupoService } from 'src/app/services/grupo.service';
-import { FrequenciaService } from 'src/app/services/frequencia.service';
 
 @Component({
   selector: 'app-professor-dashboard',
@@ -11,24 +7,8 @@ import { FrequenciaService } from 'src/app/services/frequencia.service';
 })
 export class ProfessorDashboardComponent implements OnInit {
 
-  grupos: Grupo[];
-
-  loading: boolean = true;
-
-  idEstudanteUsuarioLogado : number;
-
-  constructor(private grupoService: GrupoService, 
-              private authGuardService: AuthGuardService,
-              private frequenciaService: FrequenciaService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.idEstudanteUsuarioLogado = this.authGuardService.getIdEstudanteUsuarioLogado();
-    this.grupoService.ObterGrupoPeloEstudanteIdSemestreAtivo(this.idEstudanteUsuarioLogado).subscribe(resultado => {
-      this.grupos = resultado; 
-      this.grupos.forEach((grupo) => 
-        this.frequenciaService.ObterFrequenciaByGrupoIdByEstudanteId(grupo.id, this.idEstudanteUsuarioLogado).subscribe(resultado => {grupo.frequencia = resultado.frequencia})
-      )
-      this.loading = false;
-    });    
   }
 }
