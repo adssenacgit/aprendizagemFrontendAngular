b5f4d01f352c0ef6eee6fa90fc6b1151fbe98e01
Daniel Dantas <contatodanieldantasdev@gmail.com>
2023-10-30 09:24:31 -0300

    feat: add functions verify user role Professor and Usuario

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -50,6 +50,30 @@ export class AuthGuardService implements CanActivate {
     //  return false;
   }
 
+  VerificarProfessor(): boolean {
+    const token:string = String(sessionStorage.getItem('TokenUsuarioLogado'));
+    const tokenUsuario = this.getDecodedAccessToken(token);
+    const decodedRole = tokenUsuario['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+    if(decodedRole === 'Professor'){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  VerificarEstudante(): boolean {
+    const token:string = String(sessionStorage.getItem('TokenUsuarioLogado'));
+    const tokenUsuario = this.getDecodedAccessToken(token);
+    const decodedRole = tokenUsuario['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+    if(decodedRole === 'Usuario'){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
   getIdUsuarioLogado(): string {
     //const token = String(localStorage.getItem('TokenUsuarioLogado'));
     const token = String(sessionStorage.getItem('TokenUsuarioLogado'));
@@ -57,7 +81,7 @@ export class AuthGuardService implements CanActivate {
     const decodedName = tokenUsuario['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
     return decodedName;
   }
-    
+
   getNomeUsuarioLogado(): string {
     //const token = String(localStorage.getItem('TokenUsuarioLogado'));
     const token = String(sessionStorage.getItem('TokenUsuarioLogado'));
