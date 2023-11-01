9636e6f3ebd68eddb0c9fc39551df005d0da362b
Lucas-Santini <114113893+Lucas-Santini@users.noreply.github.com>
2023-11-01 19:10:31 -0300

    Update chapter-assunto.service.ts
    Atualização e pull request do arquivo src/app/services/chapter-assunto.service.ts que foi feito na sprint passada

--- src/app/services/chapter-assunto.service.ts
+++ src/app/services/chapter-assunto.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ChapterAssunto } from '../models/ChapterAssunto';
-import { ChapterTag } from '../models/ChapterTag';
 
 
 const httpOptions = {
@@ -18,7 +17,6 @@ const httpOptions = {
 })
 export class ChapterAssuntoService {
   url:string = environment.apiServer + 'api/ChapterAssunto';
-  javaUrl:string = "http://localhost:8080/chapter-assunto"
 
   constructor(private https: HttpClient) { }
 
@@ -27,35 +25,15 @@ export class ChapterAssuntoService {
     return this.https.get<ChapterAssunto[]>(this.url);
   }
 
-  ObterTodosJava() : Observable<ChapterAssunto[]>
+  ObterChapterAssuntoById (cursoId: number) : Observable<ChapterAssunto>
   {
-    return this.https.get<ChapterAssunto[]>(this.javaUrl);
-  }
-
-  ObterChapterAssuntoById (chapterAssuntoId: number) : Observable<ChapterAssunto>
-  {
-    const apiUrl = `${this.url}/${chapterAssuntoId}`;
-    return this.https.get<ChapterAssunto>(apiUrl);
-  }
-  ObterChapterAssuntoByIdJava (chapterAssuntoId: number) : Observable<ChapterAssunto>
-  {
-    const apiUrl = `${this.javaUrl}/${chapterAssuntoId}`;
+    const apiUrl = `${this.url}/${cursoId}`;
     return this.https.get<ChapterAssunto>(apiUrl);
   }
 
-  ObterChapterAssuntosByChapterId (chapterId: number): Observable<ChapterAssunto[]> {
-    const apiUrl = `${this.url}/filterByChapterId/${chapterId}`;
-    return this.https.get<ChapterAssunto[]>(apiUrl);
-  }
-
-  NovoChapterAssunto (chapterAssunto: ChapterAssunto): Observable<any>
+  NovoChapterAssunto (chapter: ChapterAssunto): Observable<any>
   {
-    return this.https.post<ChapterAssunto>(this.url, chapterAssunto, httpOptions);
-  }
-  
-  NovoChapterAssuntoJava (chapterAssunto: ChapterAssunto): Observable<any>
-  {
-    return this.https.post<ChapterAssunto>(this.javaUrl, chapterAssunto);
+    return this.https.post<ChapterAssunto>(this.url, chapter, httpOptions);
   }
 
   AtualizarChapterAssunto(chapterId: number, chapter: ChapterAssunto):Observable<any>
@@ -70,15 +48,9 @@ export class ChapterAssuntoService {
     return this.https.delete<ChapterAssunto>(apiUrl, httpOptions);
   }
 
-  FiltrarChapterAssunto(descricaoAssunto: string) : Observable<ChapterAssunto[]>
-  {
-    const apiUrl = `${this.url}/FiltrarChapterAssunto${descricaoAssunto}`;
+  FiltrarChapterAssunto(descricaoAssunto: string): Observable<ChapterAssunto[]> {
+    const apiUrl = `${this.url}/FiltrarChapterAssunto?descricao=${descricaoAssunto}`;
     return this.https.get<ChapterAssunto[]>(apiUrl);
   }
-
-  AssociarTagsChapterAssuntoJava(chapterAssuntoId: number, chapterTags: ChapterTag[]): Observable<any>
-  {
-    const apiUrl = `${this.javaUrl}/${chapterAssuntoId}/associar-tags`;
-    return this.https.post<ChapterAssunto>(apiUrl, chapterTags, httpOptions);
-  }
+  
 }
