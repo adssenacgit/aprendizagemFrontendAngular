4f826265d6c6902369574a841ba22630b5fd911d
Felipe Beser <felipebeserx@gmail.com>
2023-11-03 23:52:11 -0300

    feat: marcar objeto como cursado

--- src/app/services/participante.service.ts
+++ src/app/services/participante.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Participante } from '../models/Participante';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +15,13 @@ export class ParticipanteService {
     private http: HttpClient
   ) { }
 
-  obterParticipantesPorGrupoIdJava(grupoId: number) {
+  obterParticipantesPorGrupoIdJava(grupoId: number): Observable<Participante[]> {
     const apiUrl = `${this.javaUrl}/filtrar-participantes-por-grupo-id/${grupoId}`;
     return this.http.get<Participante[]>(apiUrl);
+  }
+
+  obterParticipantePorEstudanteIdPorGrupoIdJava(estudanteId: number, grupoId: number): Observable<Participante>{
+    const apiUrl = `${this.javaUrl}/filtrar-participante-por-estudante-id-por-grupo-id/${estudanteId}/${grupoId}`;
+    return this.http.get<Participante>(apiUrl);
   }
 }
